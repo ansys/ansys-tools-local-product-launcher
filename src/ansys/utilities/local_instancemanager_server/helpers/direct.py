9ae9de03bb8c445d295f8d8ce1e7824403e822f3
Dominik Gresch <greschd@users.noreply.github.com>
2022-12-08 15:03:44 +0100

    Implement restarting of servers (#10)
    Add the option to restart a server. This requires a change to the interface, in that
    the `__init__` should not start the server, and a separate `start` method implements
    that instead.

--- src/ansys/utilities/local_instancemanager_server/helpers/direct.py
+++ src/ansys/utilities/local_instancemanager_server/helpers/direct.py
@@ -8,17 +8,17 @@ from ..interface import LAUNCHER_CONFIG_T, LauncherProtocol
 
 
 class DirectLauncherBase(LauncherProtocol[LAUNCHER_CONFIG_T], ABC):
-    def __init__(
-        self,
-        process: subprocess.Popen[str],
-        stdout: TextIO,
-        stderr: TextIO,
-    ):
+    def __init__(self) -> None:
+        self._process: subprocess.Popen[str]
+        self._stdout: TextIO
+        self._stderr: TextIO
+
+    def _start(self, *, process: subprocess.Popen[str], stdout: TextIO, stderr: TextIO) -> None:
         self._process = process
         self._stdout = stdout
         self._stderr = stderr
 
-    def close(self) -> None:
+    def stop(self) -> None:
         self._process.terminate()
         self._process.wait()
         self._stdout.close()
--- src/ansys/utilities/local_instancemanager_server/server_handle.py
+++ src/ansys/utilities/local_instancemanager_server/server_handle.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import Any, Dict
+import time
+from typing import Any, Dict, Optional
 import weakref
 
 import grpc
@@ -11,41 +12,87 @@ from .interface import LAUNCHER_CONFIG_T, LauncherProtocol, ServerType
 class ServerHandle:
     def __init__(self, *, launcher: LauncherProtocol[LAUNCHER_CONFIG_T]):
         self._launcher = launcher
-
-        self._finalizer = weakref.finalize(
-            self,
-            self._launcher.close,
-        )
-        self._channels: Dict[str, grpc.Channel] = dict()
-        urls = self.urls
-        if urls.keys() != launcher.SERVER_SPEC.keys():
-            raise RuntimeError(
-                f"The URL keys '{urls.keys()}' provided by the launcher "
-                f"do not match the SERVER_SPEC keys '{launcher.SERVER_SPEC.keys()}'"
-            )
-        for key, server_type in launcher.SERVER_SPEC.items():
-            if server_type == ServerType.GRPC:
-                self._channels[key] = grpc.insecure_channel(urls[key])
+        self._finalizer: weakref.finalize
+        self._urls: Dict[str, str]
+        self._channels: Dict[str, grpc.Channel]
+        self.start()
 
     def __enter__(self) -> ServerHandle:
         return self
 
     def __exit__(self, *exc: Any) -> None:
-        self.close()
+        self.stop()
 
-    def close(self) -> None:
+    def start(self) -> None:
+        if not self.stopped:
+            raise RuntimeError("Cannot start the server, it has already been started.")
+        self._finalizer = weakref.finalize(
+            self,
+            self._launcher.stop,
+        )
+        self._launcher.start()
+        self._channels = dict()
+        urls = self.urls
+        if urls.keys() != self._launcher.SERVER_SPEC.keys():
+            raise RuntimeError(
+                f"The URL keys '{urls.keys()}' provided by the launcher "
+                f"do not match the SERVER_SPEC keys '{self._launcher.SERVER_SPEC.keys()}'"
+            )
+        for key, server_type in self._launcher.SERVER_SPEC.items():
+            if server_type == ServerType.GRPC:
+                self._channels[key] = grpc.insecure_channel(urls[key])
+
+    def stop(self) -> None:
+        if self.stopped:
+            raise RuntimeError("Cannot stop the server, it has already been stopped.")
         self._finalizer()
 
-    def check(self) -> bool:
-        return self._launcher.check()
+    def restart(self) -> None:
+        self.stop()
+        self.start()
+
+    def check(self, timeout: Optional[float] = None) -> bool:
+        return self._launcher.check(timeout=timeout)
+
+    def wait(self, timeout: float) -> None:
+        """Wait for all servers to respond.
+
+        Repeatedly checks if the server(s) are running, returning as soon
+        as they are all ready.
+
+        Parameters
+        ----------
+        timeout :
+            Wait time before raising an exception.
+
+        Raises
+        ------
+        RuntimeError :
+            In case the server still has not responded after ``timeout`` seconds.
+        """
+        start_time = time.time()
+        while time.time() - start_time <= timeout:
+            if self.check(timeout=timeout / 3):
+                break
+            else:
+                # Try again until the timeout is reached. We add a small
+                # delay s.t. the server isn't bombarded with requests.
+                time.sleep(timeout / 100)
+        else:
+            raise RuntimeError(f"The product is not running after {timeout}s.")
 
     @property
     def urls(self) -> Dict[str, str]:
         return self._launcher.urls
 
     @property
-    def closed(self) -> bool:
-        return not self._finalizer.alive
+    def stopped(self) -> bool:
+        try:
+            return not self._finalizer.alive
+        # If the server has never been started, the '_finalizer' attribute
+        # may not be defined.
+        except AttributeError:
+            return True
 
     @property
     def channels(self) -> Dict[str, grpc.Channel]:
