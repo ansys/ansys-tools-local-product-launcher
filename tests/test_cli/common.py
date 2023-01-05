58753abd099d58999942e46fec8730601e204438
Dominik Gresch <dominik.gresch@ansys.com>
2023-01-05 14:47:44 +0100

    Register test helper module for assert rewriting
    Use 'pytest.register_assert_rewrite' to improve the error message
    when the config doesn't match the expected value.

--- tests/test_cli/common.py
+++ tests/test_cli/common.py
@@ -4,4 +4,4 @@ import json
 def check_result_config(path, expected):
     with open(path) as f:
         config = json.load(f)
-    assert config == expected, f"Unexpected config: {config}"
+    assert config == expected
